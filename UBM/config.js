804a264a0ad3ee2da083553e8542a94a18f65da3
3OW <test@lehmannfelix.de>
2016-09-29 13:28:01 +0200

    updated connectors - commented and cleaned code

--- UBM/config.js
+++ UBM/config.js
@@ -5,21 +5,21 @@
 
 var config = {};
 
-// Set the port the APP schould run on
+// et the port the APP schould run on
 config.port =  '3001';
 
-// Set the DB URI
+// et the DB URI
 // example: config.dbURI = "mongodb://<dbuser>:<dbpassword>@<server>:<port>/<dbName>"
 config.dbURI = "mongodb://localhost:27017/ubm";
 
 
-// Enable / Disable Basic auth (API)
+// enable / disable Basic auth (API) (true or false)
 config.basicAuth = true;
 
-// Enable Unicorn 1.5 compatibility (basic auth REST notifications not implemented in Unicorn 1.5)
+// enable Unicorn 1.5 compatibility (basic auth REST notifications not implemented in Unicorn 1.5)
 config.unicornExemption = true;
 
-// Set session expiration time
+// set session expiration time
 config.sessionLength = 86400000;
 
 module.exports = config;
